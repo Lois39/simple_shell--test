17040cf749ff52c04b918839e8d1a035e407f06e
Lois <okerekelois2@gmail.com>
2023-04-26 15:32:48 -0700

    trying some things

--- execve.c
+++ execve.c
@@ -8,11 +8,11 @@
 void executeCommand(char **args)
 {
 	char *command = NULL;
+
 	if (args)
 	{
 		/* get the command */
 		command = args[0];
-		
 		/* execute the command with execve */
 		if (execve(command, args, NULL) == -1)
 		{
