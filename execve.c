c920580210e09f6857b7247dd227130a76128189
MariamAdisa <drmariamadisa@gmail.com>
2023-04-26 09:01:38 -0700

    removed errors

--- execve.c
+++ execve.c
@@ -1,15 +1,24 @@
 #include "main.h"
+
+/**
+ * executeCommand - executes the program referred to by pathname
+ * @args: array of pointers to strings
+ */
+
 void executeCommand(char **args)
 {
-char *command = NULL;
-if (args)
-{
-/* get the command */
-command = args[0];
-/* execute the command with execve */
-if (execve(command, args, NULL) == -1)
-{
-perror("Error:");
-}
-}
+	char *command = NULL;
+	if (args)
+	{
+		/* get the command */
+		command = args[0];
+		
+		/* execute the command with execve */
+		if (execve(command, args, NULL) == -1)
+		{
+			perror("Error:");
+		};
+
+	}
+
 }
