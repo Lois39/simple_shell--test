6d51ba081e0e9bfff3896d1f69af3a15254461c2
Lois <okerekelois2@gmail.com>
2023-04-27 15:23:41 -0700

    Made a few changes

--- shell.c
+++ shell.c
@@ -10,64 +10,71 @@
 
 int main(int ac, char *av[])
 {
-	char *temp = NULL, *temp_copy = NULL, *token;
-	size_t bytes = 10;
-	ssize_t n_read;
-	const char *delim = " \n";
-	int i, ntokens = 0;
+char *temp = NULL, *temp_copy = NULL, *token;
+size_t bytes = 10;
+ssize_t n_read;
+const char *delim = " \n";
+int i, ntokens = 0;
 
-	(void)ac;
+(void)ac;
 
-	temp = malloc(sizeof(char) * bytes);
+temp = malloc(sizeof(char) * bytes);
 
-	if (temp == NULL)
-		return (0);
+if (temp == NULL)
+return (0);
 
-	while (1)
-	{
-		printf("shell$ ");
-		n_read = getline(&temp, &bytes, stdin);
-		
-		if (n_read == -1)
-		{
-			printf("EXIT SHELL...\n");
-			exit(EXIT_SUCCESS);
-		}
+while (1)
+{
+write(1, "shell$ ", 7);
+n_read = getline(&temp, &bytes, stdin);
 
-		temp_copy = malloc(sizeof(char) * n_read);
-		if (temp_copy == NULL)
-		{
-			perror("memory allocation error");
-			return (-1);
-		}
+if (n_read == -1)
+{
+write(STDOUT_FILENO, "EXIT SHELL...\n", 14);
+exit(EXIT_SUCCESS);
 
-		strcpy(temp_copy, temp);
-		token = strtok(temp, delim);
+temp_copy = malloc(sizeof(char) * n_read);
+if (temp_copy == NULL)
+{
+perror("memory allocation error");
+return (-1);
+}
 
-		while (!token)
-		{
-			token = strtok(NULL, delim);
-			ntokens++;
-		}
-		
-		av = malloc(sizeof(char *) * ntokens);
-		token = strtok(temp_copy, delim);
+strcpy(temp_copy, temp);
+token = strtok(temp, delim);
 
-		for (i = 0; token != NULL; i++)
-		{
-			av[i] = malloc(sizeof(char) * strlen(token));
-			strcpy(av[i], token);
-			token = strtok(NULL, delim);
-		}
+while (!token)
+{
+token = strtok(NULL, delim);
+ntokens++;
+}
 
-		av[i] = NULL;
-		
-		executeCommand(av);
-	}
+av = malloc(sizeof(char *) * ntokens);
+token = strtok(temp_copy, delim);
 
-	free(av);
-	free(temp_copy);
-	free(temp);
+for (i = 0; token != NULL; i++)
+{
+av[i] = malloc(sizeof(char) * strlen(token));
+strcpy(av[i], token);
+token = strtok(NULL, delim);
+}
 
-	return (0);
+av[i] = NULL;
+
+/* Check for exit command */
+if (strcmp(av[0], "exit") == 0)
+{
+write(STDOUT_FILENO, "EXIT SHELL...\n", 14);
+exit(EXIT_SUCCESS);
+}
+
+executeCommand(av);
+}
+
+free(av);
+free(temp_copy);
+free(temp);
+
+return (0);
+}
 }
