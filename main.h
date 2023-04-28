6c1c6ef17595ce9cfff79965785fe818ce54e44f
MariamAdisa <drmariamadisa@gmail.com>
2023-04-28 11:02:23 +0100

    Trying to get path

--- main.h
+++ main.h
@@ -5,7 +5,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 void executeCommand(char **args);
+char *get_path(char *path);
 
 #endif
